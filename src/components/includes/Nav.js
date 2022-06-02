a33bd7a69b01fda0331142599476b50c7fb697de
Mohammed Shajahan <mohammedsh6236@gmail.com>
2022-06-02 18:02:04 +0530

    Add NavLink

--- src/components/includes/Nav.js
+++ src/components/includes/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 function Nav() {
     return (
@@ -7,11 +7,45 @@ function Nav() {
             <h1 className="logo"><Link to="/">Logo.</Link></h1>
             <nav>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
+                    {/* Link */}
+                    
+                    {/* <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                     <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/projects">Projects</Link></li>
+                    <li><Link to="/projects">Projects</Link></li> */}
+
+                    {/* NavLink */}
+                    <li>
+                        <NavLink 
+                            className={({isActive})=> isActive ? "active" : ""} 
+                            to="/">Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            className={({isActive})=> isActive ? "active" : ""} 
+                            to="/about">About
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            className={({isActive})=> isActive ? "active" : ""} 
+                            to="/contact">Contact
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            className={({isActive})=> isActive ? "active" : ""} 
+                            to="/profile">Profile
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            className={({isActive})=> isActive ? "active" : ""} 
+                            to="/projects">Projects
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
         </header>
