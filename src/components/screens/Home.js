815e1b900b44316ac1273e571c0a349318b014ab
Mohammed Shajahan <mohammedsh6236@gmail.com>
2022-06-02 18:11:05 +0530

    Add useNavigate

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,6 +1,12 @@
+import { useNavigate } from 'react-router-dom'
+
 function Home() {
+    const navigate = useNavigate()
     return(
-        <h1>Home Page</h1>
+        <>
+            <h1>Home Page</h1>
+            <h3 onClick={()=>navigate('/contact')}>Click me to go Contact page</h3>
+        </>
     )
 }
 
